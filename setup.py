7d6bfaa79e2ac8526a91cd99ec68b220bba0c60f
Nizar Mahmoud <nizarmah@hotmail.com>
2020-06-16 09:49:48 +0300

    Updated Django Versions to be Similar to Juniper Release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Framework :: Django',
-        'Framework :: Django :: 2.2.12',
+        'Framework :: Django :: 2.2.13',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
     ],
@@ -28,7 +28,7 @@ setup(
         'tests'
     ],
     install_requires=[
-        "django",
+        "django>=2.2, >3, <3.1",
         "edx-opaque-keys",
         "python-dateutil",
         "requests",
